5607d64b15f29f720b8790ac28bae2856ece0336
Alexey Golubev <dr.freecx@gmail.com>
2025-08-02 13:02:30 +0300

    feat: rework build.zig

--- build.zig
+++ build.zig
@@ -1,21 +1,37 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // default app
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const build_arm_app = b.option(bool, "arm", "build special arm app") orelse false;
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    if (build_arm_app) {
+        const arm_exe = b.addExecutable(.{
+            .name = "control",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/control.zig"),
+                .target = b.resolveTargetQuery(std.Target.Query {
+                    .cpu_arch = .arm,
+                    .os_tag = .linux,
+                    .abi = .musleabihf
+                }),
+                .optimize = b.standardOptimizeOption(.{}),
+                .strip = true,
+            }),
+        });
+        arm_exe.linkLibC();
+        b.installArtifact(arm_exe);
+        return;
+    }
+
+    // default app step
     const exe = b.addExecutable(.{
         .name = "gixie",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.standardTargetOptions(.{}),
+            .optimize = b.standardOptimizeOption(.{}),
+        }),
     });
     exe.linkLibC();
-
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -26,24 +42,4 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // special arm app
-    const arm_exe = b.addExecutable(.{
-        .name = "control",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/control.zig"),
-            .target = b.resolveTargetQuery(std.Target.Query {
-                .cpu_arch = .arm,
-                .os_tag = .linux,
-                .abi = .musleabihf
-            }),
-            .optimize = .ReleaseSmall,
-            .strip = true,
-        }),
-    });
-    arm_exe.linkLibC();
-    b.installArtifact(arm_exe);
-
-    const build_arm = b.step("arm", "Build arm app");
-    build_arm.dependOn(b.getInstallStep());
 }
