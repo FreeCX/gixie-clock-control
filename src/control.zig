856d5745a51a2e93b541be849038a4da72ffc803
Alexey Golubev <dr.freecx@gmail.com>
2025-08-09 19:43:10 +0300

    fix: missing param

--- src/control.zig
+++ src/control.zig
@@ -58,9 +58,9 @@ fn updateCrontab(app: []u8, config: Config, allocator: std.mem.Allocator) !void 
     const stdout = io.getStdOut().writer();
     try stdout.print("{s}\n", .{current_crontab});
     try stdout.print("# {s}\n", .{app});
-    try stdout.print("@daily {s} | crontab -\n", .{app});
+    try stdout.print("@daily {s} crontab | crontab -\n", .{app});
     try stdout.print("{d} {d} * * * {s}\n", .{ info.sunrise.minute, info.sunrise.hour, app });
-    try stdout.print("{d} {d} * * * {s}", .{ info.sunset.minute, info.sunset.hour, app });
+    try stdout.print("{d} {d} * * * {s}\n", .{ info.sunset.minute, info.sunset.hour, app });
 }
 
 fn changeBrightness(config: Config, allocator: std.mem.Allocator) !void {
